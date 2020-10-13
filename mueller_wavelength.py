b17122afd650ca56e7f11abba9cd2a23f17d57d3
Defu <edward61221@gmail.com>
2020-10-13 18:18:09 +0800

    export to excel

--- mueller_wavelength.py
+++ mueller_wavelength.py
@@ -24,6 +24,15 @@ R_value =[]
 linear_value = []
 opticalactivity_value = []
 theta_value = []
+diattenuationMatrix = []
+depolarizationMatrix = []
+retardanceMatrix = []
+for m in range(4):
+    for n in range(4):
+        exec(f'diattenuationM{m}{n}=[]')
+        exec(f'retardanceM{m}{n} =[]')
+        exec(f'depolarizationM{m}{n}=[]')
+
 for i in range(num):
     muellermatrix = np.array([[M11[i],M12[i],M13[i],M14[i]],
                              [M21[i],M22[i],M23[i],M24[i]],
@@ -34,17 +43,57 @@ for i in range(num):
     D_value += [lc.diattenuation('D_value')]
     P_value += [lc.diattenuation('P_value')]
     Delta_value += [lc.depolarization('Delta_value')]
-    #R_value += [lc.retardancevalue('R_value')]
-    #linear_value += [lc.retardancevalue('linear')]
-    #opticalactivity_value += [lc.retardancevalue('opticalactivity')]
-    #theta_value += [lc.retardancevalue('theta')]
+    diattenuationMatrix += [lc.diattenuation('matrix')]
+    depolarizationMatrix += [lc.depolarization('matrix')]
+    retardanceMatrix += [lc.retardance('matrix')]
+    # 極分解矩陣
+    for m in range(4):
+        for n in range(4):
+            exec(f'diattenuationM{m}{n} += [lc.diattenuation("matrix")[{m},{n}]]')
+            exec(f'retardanceM{m}{n}+= [lc.retardance("matrix")[{m},{n}]]')
+            exec(f'depolarizationM{m}{n} += [lc.depolarization("matrix")[{m},{n}]]')
 
-plt.plot(wavelength,D_value,'*')
-plt.plot(wavelength,P_value,'-')
-plt.plot(wavelength,Delta_value)
-plt.legend(['Diattenuation','Polarizance','Delta_value'],loc='lower left')
-plt.title('Diattenuation, polarizance and Depolarization')
-plt.xlabel('Wavelength')
-plt.ylabel('Intensity')
-plt.ylim([-1,1])
-plt.show()
+    R_value += [lc.retardancevalue('R_value')]
+    linear_value += [lc.retardancevalue('linear')]
+    opticalactivity_value += [lc.retardancevalue('opticalactivity')]
+    theta_value += [lc.retardancevalue('theta')]
+
+#運算結果儲存至Excel
+all_list = [D_value,P_value,Delta_value,R_value,linear_value,opticalactivity_value,theta_value]
+datat = pd.DataFrame(all_list)
+data = datat.T
+data.index = wavelength
+data.columns = ['D','P','Delta','R','linear','opticalactivity','theta']
+
+diattenuation  = [diattenuationM00,diattenuationM01,diattenuationM02,diattenuationM03,
+                  diattenuationM10,diattenuationM11,diattenuationM12,diattenuationM13,
+                  diattenuationM20,diattenuationM21,diattenuationM22,diattenuationM23,
+                  diattenuationM30,diattenuationM31,diattenuationM32,diattenuationM33,]
+diattenuation = pd.DataFrame(diattenuation)
+diattenuation = diattenuation.T
+diattenuation.index= wavelength
+diattenuation.columns=element
+
+retardance  = [retardanceM00,retardanceM01,retardanceM02,retardanceM03,
+                  retardanceM10,retardanceM11,retardanceM12,retardanceM13,
+                  retardanceM20,retardanceM21,retardanceM22,retardanceM23,
+                  retardanceM30,retardanceM31,retardanceM32,retardanceM33,]
+retardance = pd.DataFrame(retardance)
+retardance = retardance.T
+retardance.index= wavelength
+retardance.columns=element
+
+depolarization  = [depolarizationM00,depolarizationM01,depolarizationM02,depolarizationM03,
+                  depolarizationM10,depolarizationM11,depolarizationM12,depolarizationM13,
+                  depolarizationM20,depolarizationM21,depolarizationM22,depolarizationM23,
+                  depolarizationM30,depolarizationM31,depolarizationM32,depolarizationM33,]
+depolarization = pd.DataFrame(depolarization)
+depolarization = depolarization.T
+depolarization.index= wavelength
+depolarization.columns=element
+#儲存Excel
+with pd.ExcelWriter('output.xlsx') as writer:
+    data.to_excel(writer,sheet_name='DATA')
+    diattenuation.to_excel(writer,sheet_name='diattenuation')
+    retardance.to_excel(writer,sheet_name='retardance')
+    depolarization.to_excel(writer,sheet_name='depolarization')
